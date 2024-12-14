f77dabb8d89f34b2d2c485b205c62983f84c2497
Liam Duffy <liamdanielduffy@gmail.com>
2024-12-13 23:58:29 -0500

    finishes day 1

--- src/day1/day1.clj
+++ src/day1/day1.clj
@@ -1,17 +1,29 @@
 (ns day1.day1
   (:require [clojure.string :as str]))
 
-(defn get-pt-1-answer []
+(defn get-nums []
   (let [input (slurp "./src/day1/input.txt")
         lines (str/split-lines input)
         str-pairs (map #(str/split % #"\s+") lines)
         nums (map #(Integer/parseInt %) (flatten str-pairs))
         left-nums (take-nth 2 nums)
-        right-nums (take-nth 2 (rest nums))
-        sorted-left-nums (sort left-nums)
-        sorted-right-nums (sort right-nums)
-        distances (map #(Math/abs (- %1 %2)) sorted-right-nums sorted-left-nums)
+        right-nums (take-nth 2 (rest nums))]
+    [left-nums right-nums]))
+
+(defn get-pt-1-answer []
+  (let [[left-nums right-nums] (get-nums)
+        distances (map #(Math/abs (- %1 %2)) 
+                       (sort left-nums) 
+                       (sort right-nums))
         total-distance (reduce + distances)]
     total-distance))
 
-(get-pt-1-answer)
+(defn get-pt-2-answer []
+  (let [[left-nums right-nums] (get-nums)
+        right-freqs (frequencies right-nums)
+        left-freqs-in-right (map #(or (right-freqs %) 0) left-nums)
+        scores (map #(* %1 %2) left-freqs-in-right left-nums)
+        total-score (reduce + scores)]
+    total-score))
+
+
